7ae147fa0c2a41c2c068caa3419547ab35dc66cd
Raza Aziz <razaaziz9191@gmail.com>
2026-04-29 17:12:33 +0500

    feat: Add multi-worker Express server. Checks if master or worker process, then either creates worker processes if master OR creates Express server if worker

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cluster from "cluster";
+import os from "os";
+
+const cpuCount = os.availableParallelism();
+const port = 3000;
+
+// Flow 1 : Program starts
+// Check if worker or master process
+if (cluster.isPrimary) {
+  console.log("Master process started");
+  for (let i = 0; i < cpuCount; i++) {
+    cluster.fork();
+    console.log(`Worker ${i} started`);
+  }
+} else if (cluster.isWorker) {
+  const app = express();
+
+  app.listen(port, () => {
+    console.log(
+      `Server running at http://localhost:${port} with ${cpuCount} workers`,
+    );
+  });
+}
